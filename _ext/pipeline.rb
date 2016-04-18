bf0b9405c423b0f1fb24ea27ecf74c520dc09c6f
Michael Kruger <makruger2000@gmail.com>
2016-04-17 22:20:45 -0400

    fresh awestruct build

---
+++ _ext/pipeline.rb
@@ -0,0 +1,9 @@
+require 'bootstrap-sass'
+require 'asciidoctor'
+
+Awestruct::Extensions::Pipeline.new do
+  # extension Awestruct::Extensions::Posts.new '/news'
+  # extension Awestruct::Extensions::Indexifier.new
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+end
